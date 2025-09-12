d918341abfd24dd4bb26a3990fd5f178dd294858
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2025-09-12 20:11:35 +0530

    QR generator

---
+++ script.js
@@ -0,0 +1,19 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generatorQR(){
+    if(qrText.value.length > 0){// text na hone pr bhi default image de rha tha usse problem ko solve ken ke liye  if ka us ekiya hai
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
+    }
+    else{
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+        },1000);
+    }
+    
+}
+
+
