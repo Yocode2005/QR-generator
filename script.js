6c605faff6ae914a8e4a85cecbebd8f4c183acdc
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-05-08 21:18:51 +0530

    add function for qr generation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let imgBox = document.getElementById("imgBox"); // for getting inbox
 let qrImage = document.getElementById("qrImage"); // for getting qrImage
 let qrText = document.getElementById("qrText"); // for qr text
 
-function generatorQR(){
+function generatorQR(){ // function for creating qr
     if(qrText.value.length > 0){// text na hone pr bhi default image de rha tha usse problem ko solve ken ke liye  if ka us ekiya hai
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
     imgBox.classList.add("show-img");
