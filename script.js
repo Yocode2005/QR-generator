b0e9cb9a57e63a41c9544ae33d14c2f79f47508d
Yogesh kumar meena <yogeshchhandhwal2005@gmail.com>
2026-05-08 21:18:22 +0530

    random text for qr

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let imgBox = document.getElementById("imgBox"); // for getting inbox
 let qrImage = document.getElementById("qrImage"); // for getting qrImage
-let qrText = document.getElementById("qrText");
+let qrText = document.getElementById("qrText"); // for qr text
 
 function generatorQR(){
     if(qrText.value.length > 0){// text na hone pr bhi default image de rha tha usse problem ko solve ken ke liye  if ka us ekiya hai
